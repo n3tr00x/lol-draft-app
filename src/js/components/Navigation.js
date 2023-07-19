ebf5479fbb85e4b1fd761af7a5cf86ef27ad2896
Hubert Warchoł <n3tr00x99@gmail.com>
2023-07-19 16:48:43 +0200

    added click event on the drawButton

--- src/js/components/Navigation.js
+++ src/js/components/Navigation.js
@@ -1,4 +1,6 @@
+import { resetChampions } from '../state/handleDraw.js';
 import createHTMLElement from '../utils/createHTMLElement.js';
+import { renderChampions } from './champions/DrawnChampions.js';
 
 const Navigation = () => {
 	const saveDraftButton = createHTMLElement('button', 'Save', {
@@ -7,6 +9,15 @@ const Navigation = () => {
 
 	const drawButton = createHTMLElement('button', 'Draw', {
 		className: ['navigation__button', 'navigation__button--draw'],
+	});
+
+	drawButton.addEventListener('click', () => {
+		resetChampions();
+
+		const champions = document.querySelector('.champions');
+		const newDraft = renderChampions();
+
+		champions.appendChild(newDraft);
 	});
 
 	return createHTMLElement('nav', null, {
